0460836fcbd97dbabacc6b3ae9edd292d51e27cb
repo <prorepo@xcfd.com>
2017-02-01 09:47:14 +0000

    Revert "xmltv merge"
    This reverts commit c4ad689f9c71e19c50adfa488b82509052a025c4.

--- service.XMLTVmerge/lib/update_lists.py
+++
@@ -1,5 +0,0 @@
-import os
-from common_variables import *
-from listmerger import *
-xbmc.executebuiltin("Notification(%s,%s,%i,%s)" % ('XMLTVmerge', "Merging Lists. Wait...", 1,os.path.join(addonfolder,"icon.png")))
-m3u_merge(notification=True)
--- service.XMLTVmerge/lib/update_xml.py
+++
@@ -1,5 +0,0 @@
-import os
-from common_variables import *
-from xmltvmerger import *
-xbmc.executebuiltin("Notification(%s,%s,%i,%s)" % ('XMLTVmerge', "Merging EPG's. Wait...", 1,os.path.join(addonfolder,"icon.png")))
-xml_merge(notification=True)
